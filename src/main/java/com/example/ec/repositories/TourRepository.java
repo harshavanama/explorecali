6c7b8e12ee88f7edfdac569617e274939ca7cbb8
harsha vanama <harshavanama@MacBook-Pro.local>
2022-04-10 20:59:13 -0500

    added spring datarest annotation which disables people from modifying the data and added a dependency HAL which is similar to swagger

--- src/main/java/com/example/ec/repositories/TourRepository.java
+++ src/main/java/com/example/ec/repositories/TourRepository.java
@@ -2,9 +2,38 @@ package com.example.ec.repositories;
 
 import com.example.ec.dto.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
     List<Tour> findByTourPackageCode(String code);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
